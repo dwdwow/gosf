0929ba596b0bb88094cd99c83e6365a4113804fe
dwd <wenyi173@gmail.com>
2025-02-03 18:13:49 +0800

    update

--- ws_clt.go
+++ ws_clt.go
@@ -120,9 +120,8 @@ func (h *SimpleWsMsgHandler) Handle(msg []byte) {
 	n := time.Now()
 	t, err := time.Parse(time.RFC3339Nano, tx.Timestamp)
 	if err != nil {
-		h.logger.Error("ws message", "error", "Failed to parse timestamp", "error", err)
+		h.logger.Error("ws message", "raw", tx.Timestamp, "error", err)
 		panic(1)
-		return
 	}
 	du := n.Sub(t)
 	fmt.Println(du)
